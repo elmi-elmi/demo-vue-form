1d2322f00cf65f5dfb7ca85349ff252075efaf1b
shahrokh <shahrokhelmi@gmail.com>
2022-04-14 21:53:29 +0430

    l3-end

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <label>{{ label }}</label>
+    <input
+      v-bind="$attrs"
+      :value="modelValue"
+      :placeholder="label"
+      @input="$emit('update:modelValue',$event.target.value)"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  name: 'BaseInput',
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [String, Number]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/SimpleForm.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <h1>Create an event</h1>
+    <form>
+
+      <label>Select a category</label>
+      <select v-model="event.category">
+        <option
+          v-for="option in categories"
+          :value="option"
+          :key="option"
+          :selected="option === event.category"
+        >{{ option }}</option>
+      </select>
+
+      <h3>Name & describe your event</h3>
+
+      <BaseInput label="Title" type="text" v-model="event.title"/>
+
+      <BaseInput label="Description" type="text" v-model="event.description"/>
+
+      <h3>Where is your event?</h3>
+
+      <BaseInput label="Location" type="text" v-model="event.location"/>
+
+      <h3>Are pets allowed?</h3>
+      <div>
+        <input
+            type="radio"
+            v-model="event.pets"
+            :value="1"
+            name="pets"
+          />
+        <label>Yes</label>
+      </div>
+
+      <div>
+        <input
+          type="radio"
+          v-model="event.pets"
+          :value="0"
+          name="pets"
+        />
+        <label>No</label>
+      </div>
+
+      <h3>Extras</h3>
+      <div>
+        <input
+          type="checkbox"
+          v-model="event.extras.catering"
+          class="field"
+        />
+        <label>Catering</label>
+      </div>
+
+      <div>
+        <input
+          type="checkbox"
+          v-model="event.extras.music"
+          class="field"
+        />
+        <label>Live music</label>
+      </div>
+
+      <button class="button -fill-gradient" type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      categories: [
+        'sustainability',
+        'nature',
+        'animal welfare',
+        'housing',
+        'education',
+        'food',
+        'community'
+      ],
+      event: {
+        category: '',
+        title: '',
+        description: '',
+        location: '',
+        pets: 1,
+        extras: {
+          catering: false,
+          music: false
+        }
+      }
+    }
+  }
+}
+</script>
